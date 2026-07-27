65df11f637cafcf93b48869211ef3e93ad421fa5
Shivam16Gupta <shivamgupta1281@gmail.com>
2026-07-27 22:20:14 +0530

    Added file selection

--- frontend/src/nodes/inputNode.js
+++ frontend/src/nodes/inputNode.js
@@ -1,15 +1,22 @@
 // inputNode.js
-import { useState } from 'react';
-import { Position } from 'reactflow';
-import { BaseNode } from './baseNode';
+import { useState } from "react";
+import { Position } from "reactflow";
+import { BaseNode } from "./baseNode";
 
 export const InputNode = ({ id, data }) => {
   const [currName, setCurrName] = useState(
-    data?.inputName || id.replace('customInput-', 'input_')
+    data?.inputName || id.replace("customInput-", "input_"),
   );
-  const [inputType, setInputType] = useState(
-    data?.inputType || 'Text'
-  );
+  const [inputType, setInputType] = useState(data?.inputType || "Text");
+
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setSelectedFiles(files);
+
+    console.log("Selected Files:", files);
+  };
 
   return (
     <BaseNode
@@ -18,7 +25,7 @@ export const InputNode = ({ id, data }) => {
       handles={[
         {
           id: `${id}-value`,
-          type: 'source',
+          type: "source",
           position: Position.Right,
         },
       ]}
@@ -42,6 +49,27 @@ export const InputNode = ({ id, data }) => {
           <option value="File">File</option>
         </select>
       </label>
+
+      {inputType ==="File" && (
+        <>
+          <input type="file" multiple onChange={handleFileChange} />
+
+          {selectedFiles.length > 0 && (
+            <div>
+              <h4>Selected File:</h4>
+              <ul>
+                {selectedFiles.map((file,index)=>(
+                  <li key={index}>
+                    {file.name} ({(file.size /1024).toFixed(2)} KB)
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+        </>
+      )
+      }
     </BaseNode>
   );
 };
